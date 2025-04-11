036b13b5414bdf8c91c6cc6c3992495b829f61ff
Tolkynbayeva <workfortime37@gmail.com>
2025-04-12 03:42:54 +0500

    add core-js-objects

---
+++ core-js-objects/src/objects-tasks.js
@@ -0,0 +1,529 @@
+/* ************************************************************************************************
+ *                                                                                                *
+ * Please read the following tutorial before implementing tasks:                                   *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object        *
+ *                                                                                                *
+ ************************************************************************************************ */
+
+/**
+ * Returns shallow copy of an object.
+ *
+ * @param {Object} obj - an object to copy
+ * @return {Object}
+ *
+ * @example
+ *    shallowCopy({a: 2, b: 5}) => {a: 2, b: 5}
+ *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
+ *    shallowCopy({}) => {}
+ */
+function shallowCopy(obj) {
+  return { ...obj };
+}
+
+/**
+ * Merges array of objects into a single object. If there are overlapping keys, the values
+ * should be summed.
+ *
+ * @param {Object[]} objects - The array of objects to merge
+ * @return {Object} - The merged object
+ *
+ * @example
+ *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
+ *    mergeObjects([]) => {}
+ */
+function mergeObjects(objects) {
+  return objects.reduce((acc, obj) => {
+    return Object.entries(obj).reduce((newAcc, [key, value]) => {
+      return { ...newAcc, [key]: (newAcc[key] || 0) + value };
+    }, acc);
+  }, {});
+}
+
+/**
+ * Removes a properties from an object.
+ *
+ * @param {Object} obj - The object from which to remove the property
+ * @param {Array} keys - The keys of the properties to remove
+ * @return {Object} - The object with the specified key removed
+ *
+ * @example
+ *    removeProperties({a: 1, b: 2, c: 3}, ['b', 'c']) => {a: 1}
+ *    removeProperties({a: 1, b: 2, c: 3}, ['d', 'e']) => {a: 1, b: 2, c: 3}
+ *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
+ *
+ */
+function removeProperties(obj, keys) {
+  return Object.keys(obj).reduce((acc, key) => {
+    if (!keys.includes(key)) {
+      acc[key] = obj[key];
+    }
+    return acc;
+  }, {});
+}
+
+/**
+ * Compares two source objects. Returns true if the objects are equal and false otherwise.
+ * There are no nested objects.
+ *
+ * @param {Object} obj1 - The first object to compare
+ * @param {Object} obj2 - The second object to compare
+ * @return {boolean} - True if the objects are equal, false otherwise
+ *
+ * @example
+ *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
+ *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
+ */
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  if (keys1.length !== keys2.length) return false;
+  return keys1.every(
+    (key) =>
+      Object.prototype.hasOwnProperty.call(obj2, key) && obj1[key] === obj2[key]
+  );
+}
+
+/**
+ * Checks if the source object is empty.
+ * Returns true if the object contains no enumerable own properties, false otherwise.
+ *
+ * @param {Object} obj - The object to check
+ * @return {boolean} - True if the object is empty, false otherwise
+ *
+ * @example
+ *    isEmptyObject({}) => true
+ *    isEmptyObject({a: 1}) => false
+ */
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+/**
+ * Makes the source object immutable by preventing any changes to its properties.
+ *
+ * @param {Object} obj - The source object to make immutable
+ * @return {Object} - The immutable version of the object
+ *
+ * @example
+ *    const obj = {a: 1, b: 2};
+ *    const immutableObj = makeImmutable(obj);
+ *    immutableObj.a = 5;
+ *    console.log(immutableObj) => {a: 1, b: 2}
+ *    delete immutableObj.a;
+ *    console.log(immutableObj) => {a: 1, b: 2}
+ *    immutableObj.newProp = 'new';
+ *    console.log(immutableObj) => {a: 1, b: 2}
+ */
+function makeImmutable(obj) {
+  return Object.freeze(obj);
+}
+
+/**
+ * Returns a word from letters whose positions are provided as an object.
+ *
+ * @param {Object} lettersObject - An object where keys are letters and values are arrays of positions
+ * @return {string} - The constructed word
+ *
+ * @example
+ *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
+ *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
+ */
+function makeWord(lettersObject) {
+  const maxIndex = Math.max(...Object.values(lettersObject).flat());
+  const word = new Array(maxIndex + 1);
+  Object.entries(lettersObject).forEach(([letter, position]) => {
+    position.forEach((pos) => {
+      word[pos] = letter;
+    });
+  });
+  return word.join('');
+}
+
+/**
+ * There is a queue for tickets to a popular movie.
+ * The ticket seller sells one ticket at a time strictly in order and give the change.
+ * The ticket costs 25. Customers pay with bills of 25, 50, or 100.
+ * Initially the seller has no money for change.
+ * Return true if the seller can sell tickets, false otherwise
+ *
+ * @param {number[]} queue - The array representing the bills each customer pays with
+ * @return {boolean} - True if the seller can sell tickets to everyone, false otherwise
+ *
+ * @example
+ *    sellTickets([25, 25, 50]) => true
+ *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
+ */
+function sellTickets(queue) {
+  let bills25 = 0;
+  let bills50 = 0;
+
+  const failed = queue.some((bill) => {
+    if (bill === 25) {
+      bills25 += 1;
+    } else if (bill === 50) {
+      if (bills25 === 0) return true;
+      bills25 -= 1;
+      bills50 += 1;
+    } else if (bill === 100) {
+      if (bills50 > 0 && bills25 > 0) {
+        bills50 -= 1;
+        bills25 -= 1;
+      } else if (bills25 >= 3) {
+        bills25 -= 3;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  });
+  return !failed;
+}
+
+/**
+ * Returns the rectangle object with width and height parameters and getArea() method
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {Object}
+ *
+ * @example
+ *    const r = new Rectangle(10,20);
+ *    console.log(r.width);       // => 10
+ *    console.log(r.height);      // => 20
+ *    console.log(r.getArea());   // => 200
+ */
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+}
+
+Rectangle.prototype.getArea = function getArea() {
+  return this.width * this.height;
+};
+
+/**
+ * Returns the JSON representation of specified object
+ *
+ * @param {object} obj
+ * @return {string}
+ *
+ * @example
+ *    [1,2,3]   =>  '[1,2,3]'
+ *    { height: 10, width: 20 } => '{"height":10,"width":20}'
+ */
+function getJSON(obj) {
+  return JSON.stringify(obj);
+}
+
+/**
+ * Returns the object of specified type from JSON representation
+ *
+ * @param {Object} proto
+ * @param {string} json
+ * @return {object}
+ *
+ * @example
+ *    const r = fromJSON(Circle.prototype, '{"radius":10}');
+ *
+ */
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  Object.setPrototypeOf(obj, proto);
+  return obj;
+}
+
+/**
+ * Sorts the specified array by country name first and city name
+ * (if countries are equal) in ascending order.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [
+ *      { country: 'Russia',  city: 'Moscow' },
+ *      { country: 'Belarus', city: 'Minsk' },
+ *      { country: 'Poland',  city: 'Warsaw' },
+ *      { country: 'Russia',  city: 'Saint Petersburg' },
+ *      { country: 'Poland',  city: 'Krakow' },
+ *      { country: 'Belarus', city: 'Brest' }
+ *    ]
+ *                      =>
+ *    [
+ *      { country: 'Belarus', city: 'Brest' },
+ *      { country: 'Belarus', city: 'Minsk' },
+ *      { country: 'Poland',  city: 'Krakow' },
+ *      { country: 'Poland',  city: 'Warsaw' },
+ *      { country: 'Russia',  city: 'Moscow' },
+ *      { country: 'Russia',  city: 'Saint Petersburg' }
+ *    ]
+ */
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country === b.country) {
+      return a.city.localeCompare(b.city);
+    }
+    return a.country.localeCompare(b.country);
+  });
+}
+
+/**
+ * Groups elements of the specified array by key.
+ * Returns multimap of keys extracted from array elements via keySelector callback
+ * and values extracted via valueSelector callback.
+ * See: https://en.wikipedia.org/wiki/Multimap
+ *
+ * @param {array} array
+ * @param {Function} keySelector
+ * @param {Function} valueSelector
+ * @return {Map}
+ *
+ * @example
+ *   group([
+ *      { country: 'Belarus', city: 'Brest' },
+ *      { country: 'Russia', city: 'Omsk' },
+ *      { country: 'Russia', city: 'Samara' },
+ *      { country: 'Belarus', city: 'Grodno' },
+ *      { country: 'Belarus', city: 'Minsk' },
+ *      { country: 'Poland', city: 'Lodz' }
+ *     ],
+ *     item => item.country,
+ *     item => item.city
+ *   )
+ *            =>
+ *   Map {
+ *    "Belarus" => ["Brest", "Grodno", "Minsk"],
+ *    "Russia" => ["Omsk", "Samara"],
+ *    "Poland" => ["Lodz"]
+ *   }
+ */
+function group(array, keySelector, valueSelector) {
+  const result = new Map();
+
+  array.forEach((item) => {
+    const key = keySelector(item);
+    const value = valueSelector(item);
+    if (!result.has(key)) {
+      result.set(key, []);
+    }
+    result.get(key).push(value);
+  });
+  return result;
+}
+
+/**
+ * Css selectors builder
+ *
+ * Each complex selector can consists of type, id, class, attribute, pseudo-class
+ * and pseudo-element selectors:
+ *
+ *    element#id.class[attr]:pseudoClass::pseudoElement
+ *              \----/\----/\----------/
+ *              Can be several occurrences
+ *
+ * All types of selectors can be combined using the combination ' ','+','~','>' .
+ *
+ * The task is to design a single class, independent classes or classes hierarchy
+ * and implement the functionality to build the css selectors using the provided cssSelectorBuilder.
+ * Each selector should have the stringify() method to output the string representation
+ * according to css specification.
+ *
+ * Provided cssSelectorBuilder should be used as facade only to create your own classes,
+ * for example the first method of cssSelectorBuilder can be like this:
+ *   element: function(value) {
+ *       return new MySuperBaseElementSelector(...)...
+ *   },
+ *
+ * The design of class(es) is totally up to you, but try to make it as simple,
+ * clear and readable as possible.
+ *
+ * @example
+ *
+ *  const builder = cssSelectorBuilder;
+ *
+ *  builder.id('main').class('container').class('editable').stringify()
+ *    => '#main.container.editable'
+ *
+ *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify()
+ *    => 'a[href$=".png"]:focus'
+ *
+ *  builder.combine(
+ *      builder.element('div').id('main').class('container').class('draggable'),
+ *      '+',
+ *      builder.combine(
+ *          builder.element('table').id('data'),
+ *          '~',
+ *           builder.combine(
+ *               builder.element('tr').pseudoClass('nth-of-type(even)'),
+ *               ' ',
+ *               builder.element('td').pseudoClass('nth-of-type(even)')
+ *           )
+ *      )
+ *  ).stringify()
+ *    => 'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
+ *
+ *  For more examples see unit tests.
+ */
+
+class CssSelector {
+  constructor() {
+    this.selector = {
+      element: '',
+      id: '',
+      classes: [],
+      attributes: [],
+      pseudoClasses: [],
+      pseudoElement: '',
+    };
+    this.order = [];
+  }
+
+  checkOrder(type) {
+    const orderRules = [
+      'element',
+      'id',
+      'class',
+      'attr',
+      'pseudoClass',
+      'pseudoElement',
+    ];
+
+    if (
+      this.order.includes(type) &&
+      (type === 'element' || type === 'id' || type === 'pseudoElement')
+    ) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more than once inside the selector'
+      );
+    }
+
+    if (
+      this.order.length > 0 &&
+      orderRules.indexOf(this.order[this.order.length - 1]) >
+        orderRules.indexOf(type)
+    ) {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'
+      );
+    }
+
+    this.order.push(type);
+  }
+
+  element(value) {
+    this.checkOrder('element');
+    const newSelector = this.clone();
+    newSelector.selector.element = value;
+    return newSelector;
+  }
+
+  id(value) {
+    this.checkOrder('id');
+    const newSelector = this.clone();
+    newSelector.selector.id = `#${value}`;
+    return newSelector;
+  }
+
+  class(value) {
+    this.checkOrder('class');
+    const newSelector = this.clone();
+    newSelector.selector.classes.push(`.${value}`);
+    return newSelector;
+  }
+
+  attr(value) {
+    this.checkOrder('attr');
+    const newSelector = this.clone();
+    newSelector.selector.attributes.push(`[${value}]`);
+    return newSelector;
+  }
+
+  pseudoClass(value) {
+    this.checkOrder('pseudoClass');
+    const newSelector = this.clone();
+    newSelector.selector.pseudoClasses.push(`:${value}`);
+    return newSelector;
+  }
+
+  pseudoElement(value) {
+    this.checkOrder('pseudoElement');
+    const newSelector = this.clone();
+    newSelector.selector.pseudoElement = `::${value}`;
+    return newSelector;
+  }
+
+  static combine(selector1, combinator, selector2) {
+    const newSelector = new CssSelector();
+    newSelector.combined = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return newSelector;
+  }
+
+  stringify() {
+    if (this.combined) {
+      return this.combined;
+    }
+    return (
+      this.selector.element +
+      this.selector.id +
+      this.selector.classes.join('') +
+      this.selector.attributes.join('') +
+      this.selector.pseudoClasses.join('') +
+      this.selector.pseudoElement
+    );
+  }
+
+  clone() {
+    const newSelector = new CssSelector();
+    newSelector.selector = JSON.parse(JSON.stringify(this.selector)); // Глубокая копия
+    newSelector.order = [...this.order];
+    return newSelector;
+  }
+}
+
+const cssSelectorBuilder = {
+  element(value) {
+    return new CssSelector().element(value);
+  },
+
+  id(value) {
+    return new CssSelector().id(value);
+  },
+
+  class(value) {
+    return new CssSelector().class(value);
+  },
+
+  attr(value) {
+    return new CssSelector().attr(value);
+  },
+
+  pseudoClass(value) {
+    return new CssSelector().pseudoClass(value);
+  },
+
+  pseudoElement(value) {
+    return new CssSelector().pseudoElement(value);
+  },
+
+  combine(selector1, combinator, selector2) {
+    return CssSelector.combine(selector1, combinator, selector2);
+  },
+};
+
+module.exports = {
+  shallowCopy,
+  mergeObjects,
+  removeProperties,
+  compareObjects,
+  isEmptyObject,
+  makeImmutable,
+  makeWord,
+  sellTickets,
+  Rectangle,
+  getJSON,
+  fromJSON,
+  group,
+  sortCitiesArray,
+  cssSelectorBuilder,
+};
